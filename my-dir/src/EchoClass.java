67f5269248e3df7f078338858b8c29114dff8549
Paddez <dave@paddez.com>
2016-10-01 11:52:00 -0700

    Adding something silly

--- my-dir/src/EchoClass.java
+++ my-dir/src/EchoClass.java
@@ -1,10 +1,12 @@
 package com.paddez;
 
 public class EchoClass {
-
-  int CONSTANT = 5;
-
   public EchoClass(){
+  }
+
+  public String returnHex(String decimal) {
+    String hex = Integer.toHexString(Integer.parseInt(decimal));
+    return "0x"+hex.toUpperCase();
   }
 
   public String returnString(String toReturn) {
